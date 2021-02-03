c2ef6d328953eeb1b861ded4599a859bd9335454
Diogo Marques <d.marques@ua.pt>
2021-02-03 17:32:06 +0000

    Rotation in img Rec

--- imgRec_v3_copy.cpp
+++ imgRec_v3_copy.cpp
@@ -9,6 +9,24 @@ using namespace std;
 
 void normalizeAndCompare(vector<Mat> templates, vector<Mat> samples);
 
+Mat rotate(Mat src, double angle);
+
+Mat rotate(Mat src, double angle)   //rotate function returning mat object with parametres imagefile and angle    
+{
+    //get rotation matrix for rotating the image around its center in pixel coordinates
+    Point2f center((src.cols-1)/2.0, (src.rows-1)/2.0);
+    Mat rot = cv::getRotationMatrix2D(center, angle, 1.0);
+    // determine bounding rectangle, center not relevant
+    Rect2f bbox = cv::RotatedRect(cv::Point2f(), src.size(), angle).boundingRect2f();
+    // adjust transformation matrix
+    rot.at<double>(0,2) += bbox.width/2.0 - src.cols/2.0;
+    rot.at<double>(1,2) += bbox.height/2.0 - src.rows/2.0;
+
+    Mat dst;
+    warpAffine(src, dst, rot, bbox.size());
+    return dst;         //returning Mat object for output image file
+}
+
 int main(){
 
     //Read input image in gray scale
@@ -20,7 +38,7 @@ int main(){
 
     Mat frame, fgMask, diff_im, blurredImage, im_th, im_out, greyMat, image;
 
-    frame = imread("imgP2.png", IMREAD_COLOR);
+    frame = imread("imgP.png", IMREAD_COLOR);
 
 
     bitwise_xor(b,frame,diff_im);
@@ -42,21 +60,21 @@ int main(){
     Canny(image, thresholdedImage, 40, 85, 5);
 
     //To store contours
-    vector<vector<Point>> contours;
+    vector<vector<Point>> contours(6), contours1;
     
     //To store hierarchy(nestedness)
     vector<Vec4i> hierarchy;
 
     //Find contours
     //findContours(thresholdedImage, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-    findContours(thresholdedImage, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0,0));
+    findContours(thresholdedImage, contours1, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0,0));
 
-    for( size_t i = 0; i< contours.size(); i++ ){
-        drawContours(image, contours, i, Scalar(0), -1);
+    for( size_t i = 0; i< contours1.size(); i++ ){
+        drawContours(image, contours1, i, Scalar(0), -1);
     }    
 
     Canny(image, thresholdedImage, 40, 85, 5);
-    findContours(thresholdedImage, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0,0));
+    findContours(thresholdedImage, contours1, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0,0));
 
     //Output image to draw contours on
     Mat output;
@@ -64,10 +82,16 @@ int main(){
 
     RNG rng(12345);
 
+    int k = 0;
+
     double epsilon;
-    for( size_t i = 0; i< contours.size(); i++ ){
-        epsilon = 0.02*arcLength(contours[i], true);
-        approxPolyDP(contours[i], contours[i], epsilon, true);
+    for( size_t i = 0; i< contours1.size(); i++ ){
+        if (contours1[i].size() > 30){
+            epsilon = 0.02*arcLength(contours1[i], true);
+            approxPolyDP(contours1[i], contours[k], epsilon, true);
+            k++;
+        }
+
     }
 
     // Load Templates
@@ -82,6 +106,7 @@ int main(){
     vector<Mat> res;
     Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
     for( size_t i = 0; i< contours.size(); i++ ){
+        cout << "Size " << i << ": " << contours[i].size() << endl;
         Mat t = Mat::zeros(thresholdedImage.size(), CV_8UC3);
         drawContours(t, contours, i, color, CV_FILLED);
         // for (int j = 0; j < contours[i].size(); ++j){
@@ -164,16 +189,32 @@ int main(){
 
 
 void normalizeAndCompare(vector<Mat> templates, vector<Mat> samples){
-    double result;
+    double result_rotate;
     vector<String> names{"F", "L", "N", "P", "U", "X"};
     Mat tmp1, tmp2;
-
+    vector<double> result;
     for(size_t i = 0; i < templates.size(); i++){
+        vector<double> result;
         cout << names[i] << endl;
         for(size_t j = 0; j < samples.size(); j++){
-            result = (matchShapes(templates[i], samples[j], 1, 0.0));
-            cout << "Result " << j << ": " << result << endl;
+            result_rotate = (matchShapes(templates[i], samples[j], 1, 0.0));
+            double temp_result;
+            int best_angle = 0;
+            //rotate and get the best angle
+            for(int ang = 1; ang < 361; ang++){
+                temp_result = (matchShapes(templates[i], rotate(samples[j], ang), 1, 0.0)); 
+                if(temp_result<result_rotate){
+                    result_rotate = temp_result;
+                    best_angle = ang;
+                }
+            }
+
+            result.push_back( result_rotate);
+            cout << "Result " << j << ": " << result_rotate << " best ang " << best_angle << endl;
         }
+        double minElementIndex = std::min_element(result.begin(),result.end()) - result.begin();
+        double minElement = *std::min_element(result.begin(), result.end());
+        cout << "MIN INDEX " << minElementIndex << " : " << minElement << endl;
     }
 }
 
